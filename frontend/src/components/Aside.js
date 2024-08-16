e0c3b02a48b94d04c8b4a7032bb53cb95042aab0
unknown <oscarorozco0215@gmail.com>
2024-08-16 17:56:01 -0500

    cambios menu principal

--- frontend/src/components/Aside.js
+++ frontend/src/components/Aside.js
@@ -1,10 +1,9 @@
 // src/components/Aside.js
 import React from 'react';
 import { useSidebar } from './SidebarContext';
-import profileImage from '../assets/usuarios/admin/191.jpg';
 import logoClosedImage from '../assets/plantilla/logo11.png';
 import logoOpenImage from '../assets/plantilla/logo_final_2.png';
-import './Aside.css'; // Importa el CSS del Sidebar
+import './Aside.css';
 
 const Aside = () => {
   const { isOpen } = useSidebar();
@@ -27,89 +26,49 @@ const Aside = () => {
 
   const perfil = sesion.user.perfil;
 
+  const MenuItem = ({ href, icon, title }) => (
+    <li className="nav-item" title={title}>
+      <a href={href} className="nav-link">
+        <i className={`nav-icon ${icon}`}></i>
+        <p>{title}</p>
+      </a>
+    </li>
+  );
+
   return (
     <aside className={`main-sidebar sidebar-dark-primary elevation-4 ${isOpen ? 'sidebar-open' : 'sidebar-closed'}`}>
       <a href="/inicio" className="brand-link">
-        <img src={isOpen ? logoOpenImage : logoClosedImage} alt="Logo" className="image" />
+        <img 
+          src={isOpen ? logoOpenImage : logoClosedImage} 
+          alt="Logo" 
+          className={`brand-image ${isOpen ? 'logo-open' : 'logo-closed'}`} 
+        />
       </a>
       <div className="sidebar">
-        {/* <div className="user-panel mt-3 pb-3 mb-3 d-flex">
-          <div className="image">
-            <img src={profileImage} className="img-circle elevation-2" alt="User Image" />
-          </div>
-          <div className="info">
-            <a href="#" className="d-block">{sesion.user.nombre}</a>
-          </div>
-        </div> */}
         <nav className="mt-2">
           <ul className="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
             {(perfil === "Administrador" || perfil === "Especial" || perfil === "Vendedor") && (
               <>
-                <li className="nav-item" title="Inicio">
-                  <a href="inicio" className="nav-link">
-                    <i className="nav-icon bx bx-home"></i>
-                    <p>Inicio</p>
-                  </a>
-                </li>
+                <MenuItem href="inicio" icon="bx bx-home" title="Inicio" />
                 {perfil === "Administrador" && (
-                  <li className="nav-item" title="Usuarios">
-                    <a href="usuarios" className="nav-link">
-                      <i className="nav-icon bx bx-user"></i>
-                      <p>Usuarios</p>
-                    </a>
-                  </li>
+                  <MenuItem href="usuarios" icon="bx bx-user" title="Usuarios" />
                 )}
                 {(perfil === "Administrador" || perfil === "Especial") && (
                   <>
-                    <li className="nav-item" title="Tarifas">
-                      <a href="categorias" className="nav-link">
-                        <i className="nav-icon bx bx-money"></i>
-                        <p>Tarifas</p>
-                      </a>
-                    </li>
-                    <li className="nav-item" title="Ingresos">
-                      <a href="productos" className="nav-link">
-                        <i className="nav-icon bx bx-trending-up"></i>
-                        <p>Ingresos</p>
-                      </a>
-                    </li>
-                    <li className="nav-item" title="Salidas">
-                      <a href="salidas" className="nav-link">
-                        <i className="nav-icon bx bx-exit"></i>
-                        <p>Salidas</p>
-                      </a>
-                    </li>
+                    <MenuItem href="categorias" icon="bx bx-money" title="Tarifas" />
+                    <MenuItem href="productos" icon="bx bx-trending-up" title="Ingresos" />
+                    <MenuItem href="salidas" icon="bx bx-exit" title="Salidas" />
                   </>
                 )}
                 {(perfil === "Administrador" || perfil === "Vendedor") && (
                   <>
-                    <li className="nav-item" title="Abonados">
-                      <a href="clientes" className="nav-link">
-                        <i className="nav-icon bx bx-group"></i>
-                        <p>Abonados</p>
-                      </a>
-                    </li>
-                    <li className="nav-item" title="Caja">
-                      <a href="cajas" className="nav-link">
-                        <i className="nav-icon bx bx-cart"></i>
-                        <p>Caja</p>
-                      </a>
-                    </li>
-                    <li className="nav-item" title="Pagos">
-                      <a href="crear-venta" className="nav-link">
-                        <i className="nav-icon bx bx-dollar"></i>
-                        <p>Pagos</p>
-                      </a>
-                    </li>
+                    <MenuItem href="clientes" icon="bx bx-group" title="Abonados" />
+                    <MenuItem href="cajas" icon="bx bx-cart" title="Caja" />
+                    <MenuItem href="crear-venta" icon="bx bx-dollar" title="Pagos" />
                   </>
                 )}
                 {perfil === "Administrador" && (
-                  <li className="nav-item" title="Reporte ventas">
-                    <a href="reportes" className="nav-link">
-                      <i className="nav-icon bx bx-line-chart"></i>
-                      <p>Reporte ventas</p>
-                    </a>
-                  </li>
+                  <MenuItem href="reportes" icon="bx bx-line-chart" title="Reporte ventas" />
                 )}
               </>
             )}
